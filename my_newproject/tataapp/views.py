cf3058f0c9d783698f86887ec42ae81f8e117b17
sunilprajapati661 <48149575+sunilprajapati661@users.noreply.github.com>
2019-03-02 14:29:47 +0530

    add new project

---
+++ my_newproject/tataapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(HttpRequast):
+    return HttpResponse("<h1>Hello MR. sunil Prajapati</h1")
+
